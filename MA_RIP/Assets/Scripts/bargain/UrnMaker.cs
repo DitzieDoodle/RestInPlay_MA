8a1e2a85373b30df173ad33c877a8b7fd9a308bb
DitzieDoodle <nikole.ba@gmx.de>
2026-07-19 16:27:50 +0200

    Added UrnMaker script Logik and sprites

---
+++ MA_RIP/Assets/Scripts/bargain/UrnMaker.cs
@@ -0,0 +1,133 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class UrnMaker : MonoBehaviour
+{
+    [Header("Sprites")]
+    public Sprite metal ;
+    public Sprite wood;
+    public Sprite ceramic;
+    public Sprite jar;
+
+    [Header("Decor")]
+    public Sprite whoops;
+    public Sprite cross;
+    public Sprite flower;
+    public Sprite star;
+    public Sprite heart;
+
+    [Header("Sounds")]
+
+    public AudioSource audioSource;
+
+    public AudioClip metalSFX;
+    public AudioClip woodSFX;
+    public AudioClip ceramicSFX;
+    public AudioClip jarSFX;
+
+    public AudioClip whoopsSFX;
+    public AudioClip crossSFX;
+    public AudioClip flowerSFX;
+    public AudioClip starSFX;
+    public AudioClip heartSFX;
+
+
+
+    private SpriteRenderer UrnRenderer;
+    private SpriteRenderer DecorRenderer;
+    void Awake()
+    {
+        UrnRenderer = GetComponent<SpriteRenderer>();
+        DecorRenderer = GetComponent<SpriteRenderer>();
+
+
+        // AudioSource entweder von diesem GameObject oder neu erstellen
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    // Urn sprite changer
+    public void SetSpriteMetal()
+    {
+        if (metal == null || UrnRenderer == null) return;
+        UrnRenderer.sprite = metal;
+
+        audioSource.clip = metalSFX;
+        audioSource.Play();
+    }
+
+    public void SetSpriteWood()
+    {
+        if (wood == null || UrnRenderer == null) return;
+        UrnRenderer.sprite = wood;
+
+        audioSource.clip = woodSFX;
+        audioSource.Play();
+    }
+
+    public void SetSpriteCeramic()
+    {
+        if (ceramic == null || UrnRenderer == null) return;
+        UrnRenderer.sprite = ceramic;
+
+        audioSource.clip = ceramicSFX;
+        audioSource.Play();
+    }
+
+    public void SetSpriteJar()
+    {
+        if (jar == null || UrnRenderer == null) return;
+        UrnRenderer.sprite = jar;
+
+        audioSource.clip = jarSFX;
+        audioSource.Play();
+    }
+
+
+    // decor sprite changer
+    public void SetWhoops()
+    {
+        if (whoops == null || DecorRenderer == null) return;
+        DecorRenderer.sprite = jar;
+
+        audioSource.clip = whoopsSFX;
+        audioSource.Play();
+    }
+    public void SetCross()
+    {
+        if (cross == null || DecorRenderer == null) return;
+        DecorRenderer.sprite = jar;
+
+        audioSource.clip = crossSFX;
+        audioSource.Play();
+    }
+
+    public void SetFlower()
+    {
+        if (flower == null || DecorRenderer == null) return;
+        DecorRenderer.sprite = jar;
+
+        audioSource.clip = flowerSFX;
+        audioSource.Play();
+    }
+    public void SetStar()
+    {
+        if (star == null || DecorRenderer == null) return;
+        DecorRenderer.sprite = jar;
+
+        audioSource.clip = starSFX;
+        audioSource.Play();
+    }
+    public void SetHeart()
+    {
+        if (heart == null || DecorRenderer == null) return;
+        DecorRenderer.sprite = jar;
+
+        audioSource.clip = heartSFX;
+        audioSource.Play();
+    }
+
+}
