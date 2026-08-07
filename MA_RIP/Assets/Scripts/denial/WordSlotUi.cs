e896282c4bbd7e187de85073502d5c7ee94d1d23
DitzieDoodle <nikole.ba@gmx.de>
2026-08-07 13:04:15 +0200

    added feedbacks to denial minigame

--- MA_RIP/Assets/Scripts/denial/WordSlotUi.cs
+++ MA_RIP/Assets/Scripts/denial/WordSlotUi.cs
@@ -1,14 +1,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+
 public class WordSlotUi : MonoBehaviour
 {
     public bool HasRightWord { get; private set; } = false;
     public UnityEvent OnRightWordSelectedEvent = new();
-
     public List<WordUi> wordUis = new List<WordUi>();
     public WordUi CurrentWordUi { get; private set; } = null;
-
 
     void Start()
     {
@@ -20,12 +19,11 @@ public class WordSlotUi : MonoBehaviour
 
     public void OnWordSelected(WordUi selectedWord)
     {
-        // Hier können Sie die Logik implementieren, wenn ein Wort ausgewählt wird.
-        // Zum Beispiel könnten Sie überprüfen, ob das ausgewählte Wort korrekt ist.
         if (selectedWord.IsRightWord)
         {
             HasRightWord = true;
             CurrentWordUi = selectedWord;
+            selectedWord.PlayCorrectFeedback();
             OnRightWordSelectedEvent?.Invoke();
         }
         else
--- MA_RIP/Assets/Scripts/denial/WordUi.cs
+++ MA_RIP/Assets/Scripts/denial/WordUi.cs
@@ -1,58 +1,78 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using DG.Tweening;
 using TMPro;
 
-public class WordUi : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+public class WordUi : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler, IPointerEnterHandler, IPointerExitHandler
 {
+    [Header("Movement")]
     [SerializeField] float snapDistance = 60f;
     [SerializeField] bool isMoveable = true;
-
     public bool IsRightWord = false;
+
+    [Header("Audio Feedback")]
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip correctSfx;
+    [SerializeField] AudioClip wrongSfx;
+
+    [Header("Color Feedback")]
+    [SerializeField] Image targetImage;
+    [SerializeField] Color normalColor = Color.white;
+    [SerializeField] Color hoverColor = new Color(1f, 1f, 1f, 1f);
+    [SerializeField] Color dragColor = new Color(1f, 1f, 1f, 1f);
 
     public string GetWordText()
     {
         return wordText != null ? wordText.text : gameObject.name;
     }
-    TMP_Text wordText;
 
+    TMP_Text wordText;
     WordSlotUi parentSlot;
     RectTransform rectTransform;
     RectTransform parentSlotRectTransform;
     Canvas canvas;
     CanvasGroup canvasGroup;
-
     bool isDragging;
+    bool isHovering;
     bool isSnappedToParentSlot;
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvas = GetComponentInParent<Canvas>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        wordText = GetComponentInChildren<TMP_Text>();
+
+        if (targetImage == null)
+        {
+            targetImage = GetComponent<Image>();
+        }
+        if (audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        UpdateVisualState();
+    }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        if (!isMoveable)
-        {
-            return;
-        }
-        if (eventData.button != PointerEventData.InputButton.Left)
-        {
-            return;
-        }
+        if (!isMoveable) return;
+        if (eventData.button != PointerEventData.InputButton.Left) return;
 
         isDragging = true;
         isSnappedToParentSlot = false;
+        UpdateVisualState();
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        if (!isMoveable)
-        {
-            return;
-        }
-
-        if (eventData.button != PointerEventData.InputButton.Left)
-        {
-            return;
-        }
+        if (!isMoveable) return;
+        if (eventData.button != PointerEventData.InputButton.Left) return;
 
         isDragging = false;
+        UpdateVisualState();
 
         if (isSnappedToParentSlot && parentSlot != null)
         {
@@ -60,18 +80,24 @@ public class WordUi : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDr
         }
     }
 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isHovering = true;
+        UpdateVisualState();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isHovering = false;
+        UpdateVisualState();
+    }
+
     public void OnDrag(PointerEventData eventData)
     {
-        if (!isDragging || !isMoveable)
-        {
-            return;
-        }
+        if (!isDragging || !isMoveable) return;
 
         Vector3 targetWorldPosition;
-        if (!TryGetPointerWorldPosition(eventData, out targetWorldPosition))
-        {
-            return;
-        }
+        if (!TryGetPointerWorldPosition(eventData, out targetWorldPosition)) return;
 
         rectTransform.position = targetWorldPosition;
 
@@ -83,7 +109,6 @@ public class WordUi : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDr
 
         float distanceToParentSlot = Vector2.Distance(rectTransform.position, parentSlotRectTransform.position);
         isSnappedToParentSlot = distanceToParentSlot <= snapDistance;
-
         if (isSnappedToParentSlot)
         {
             rectTransform.position = parentSlotRectTransform.position;
@@ -96,16 +121,28 @@ public class WordUi : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDr
         parentSlotRectTransform = slot != null ? slot.GetComponent<RectTransform>() : null;
     }
 
-    void Awake()
+    // Ruf diese Methode von WordSlotUi auf, wenn das Wort richtig ist
+    public void PlayCorrectFeedback()
     {
-        rectTransform = GetComponent<RectTransform>();
-        canvas = GetComponentInParent<Canvas>();
-        canvasGroup = GetComponent<CanvasGroup>();
-        wordText = GetComponentInChildren<TMP_Text>();
+        if (audioSource != null && correctSfx != null)
+        {
+            audioSource.PlayOneShot(correctSfx);
+        }
+    }
+
+    // Ruf diese Methode von WordSlotUi auf, wenn das Wort falsch ist
+    public void PlayWrongFeedback()
+    {
+        if (audioSource != null && wrongSfx != null)
+        {
+            audioSource.PlayOneShot(wrongSfx);
+        }
     }
 
     public void Deny()
     {
+        PlayWrongFeedback();
+
         rectTransform.DOShakePosition(0.5f, 10f, 20, 90f, false, true).OnComplete(() =>
         {
             canvasGroup.DOFade(0f, 0.5f).OnComplete(() =>
@@ -115,32 +152,33 @@ public class WordUi : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDr
         });
     }
 
+    void UpdateVisualState()
+    {
+        if (targetImage == null) return;
+
+        if (isDragging)
+        {
+            targetImage.color = dragColor;
+        }
+        else if (isHovering)
+        {
+            targetImage.color = hoverColor;
+        }
+        else
+        {
+            targetImage.color = normalColor;
+        }
+    }
+
     bool TryGetPointerWorldPosition(PointerEventData eventData, out Vector3 worldPosition)
     {
         RectTransform canvasRect = canvas != null ? canvas.rootCanvas.transform as RectTransform : null;
         RectTransform referenceRect = canvasRect != null ? canvasRect : rectTransform;
-
         if (RectTransformUtility.ScreenPointToWorldPointInRectangle(referenceRect, eventData.position, eventData.pressEventCamera, out worldPosition))
         {
             return true;
         }
-
         worldPosition = rectTransform.position;
         return false;
     }
-
-
-    // void OnDrawGizmos()
-    // {
-    //     if (IsRightWord)
-    //     {
-    //         Gizmos.color = Color.green;
-    //     }
-    //     else
-    //     {
-    //         Gizmos.color = Color.red;
-    //     }
-
-    //     Gizmos.DrawWireCube(transform.position, transform.localScale);
-    // }
 }
